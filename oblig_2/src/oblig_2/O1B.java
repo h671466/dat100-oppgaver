c64515ff80817713d6b7caf4f5b66bda6de3043e
h671466 <671466@stud.hvl.no>
2022-09-27 06:47:26 +0200

    O1B-D

--- oblig_2/src/oblig_2/O1B.java
+++ oblig_2/src/oblig_2/O1B.java
@@ -59,12 +59,41 @@ public class O1B {
 	}
 	
 	
+	public static boolean erLik(int[][] mat1, int[][] mat2) {
+		
+		if(mat1.length != mat2.length || mat1[0].length != mat2[0].length) {
+			
+			return false;
+			
+		}
+		
+		
+		for (int rad = 0; rad < mat1.length; rad++) {
+			
+			for (int kollonne = 0; kollonne < mat1[rad].length; kollonne++) {
+				
+				if(mat1[rad][kollonne] != mat2[rad][kollonne]) {
+					
+					return false;
+					
+				}
+				
+		    }
+			
+		}
+		
+		return true;
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      int tall = 3;
 		int[][] matrise = {{1,2,3}, {4,5,6}, {7,8,9}};
+		int[][] matrise2 = {{1,2}, {4,5}, {7,8}};
 
-		System.out.println(tilStreng(matrise));
+		System.out.println(erLik(matrise, matrise2));
 	}
 
 }
